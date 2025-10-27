cc8b66a88e6f21e536854911bb49ef8df8499274
Rodrigo Ferreira Aleixo <rodrigo.f.aleixo@gmail.com>
2025-10-27 17:32:34 -0300

    Initializing the repository with new configurations

--- problems/1076_progress/main.cpp
+++ problems/1076_progress/main.cpp
@@ -5,71 +5,62 @@
 
 using namespace std;
 
-void solve(vector<list<int>> adjacency, int start, int& cont){
-    
-    for(int i = 0; i < adjacency[start].size(); i++){
-        cont++; 
+void solve(vector<pair<vector<int>, int>>& adjacency_list, int start, int& counter){
+    if (adjacency_list[start].second < 1)
+    {
+        adjacency_list[start].second = 1;
+        for (size_t i = 0; i < adjacency_list[start].first.size(); i++)
+        {
+            /* code */
+        }
+        
     }
-}
-
-void recursivity(int index){
-
+    
+    
+    adjacency_list[start].second = 2;
 }
 
 int main(){
 
+    /*in a adjacency list, we need to know the v what point, and check if the v alredy checks*/
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
+    // v what points ; check if alredy pass into this vertex
+    vector<pair<vector<int>, int>> adjacency_list;
+    int loop, start, temp_node1, temp_node2, v, a;
 
-    vector<list<int>> adjacency;
-    
-    int loop;
-    int start, temp_node1, temp_node2, vertices, arestas;
     cin >> loop;
 
-    while (loop--)
-    {
-        adjacency.clear();
-    
-
-        cin >> start;
-        cin >> vertices >> arestas;
-
-        adjacency.resize(vertices);
+    while(loop--){
         
-        for(int i = 0; i < arestas; i++){
-            cin >> temp_node1 >> temp_node2;
+        cin >> start;
+        cin >> v >> a;
 
-            if (temp_node1 < temp_node2)
-            {
+        adjacency_list.resize(v);
+        for(int i = 0; i < a; i++){
+            cin >> temp_node1 >> temp_node2;
+            if(temp_node1 < temp_node2){
                 int aux = temp_node1;
                 temp_node1 = temp_node2;
                 temp_node2 = aux;
-            }
+            } 
 
-            bool exist;
-            for(auto& num : adjacency[temp_node1]){
-                if (num == temp_node2)
-                {
-                    exist = true;
-                }
-                
-            }
-
-            if (!exist)
+            bool is_in;
+            for (size_t j = 0; j < adjacency_list[temp_node1].first.size(); j++)
             {
-                adjacency[temp_node1].push_back(temp_node2);
+                if(temp_node2 == adjacency_list[temp_node1].first[j]){
+                    is_in = true;
+                }
             }
             
-            
-            
-            adjacency[temp_node1].push_back(temp_node2);
+            if(!is_in) adjacency_list[temp_node1].first.push_back(temp_node2);
         }
 
-        int cont = 0;
-        solve(adjacency, start, cont);
+        adjacency_list.clear();
 
     }
+
+    
     
 
     return 0;
